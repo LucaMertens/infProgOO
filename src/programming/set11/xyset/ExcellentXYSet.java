66deddbc44df145650467d87e9f76a52834d2d5c
Luca Mertens <luca.mertens00@gmail.com>
2020-01-28 22:09:59 +0100

    Started working on 11.1

---
+++ src/programming/set11/xyset/ExcellentXYSet.java
@@ -0,0 +1,19 @@
+package programming.set11.xyset;
+
+/**
+ * ExcellentXYSet
+ */
+public class ExcellentXYSet<E> implements XYSet<E> {
+
+
+    @Override
+    public boolean add(E element) {
+        if (element == null) {
+            throw new IllegalArgumentException("The input was null.");
+        }
+
+        if ()
+
+    }
+
+}
