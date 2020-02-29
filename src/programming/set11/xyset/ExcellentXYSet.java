d86301057083ce4ef5b6fd212d4f455be4914e92
Luca Mertens <luca.mertens00@gmail.com>
2020-02-29 12:41:14 +0100

    Finished Set11

--- src/programming/set11/xyset/ExcellentXYSet.java
+++ src/programming/set11/xyset/ExcellentXYSet.java
@@ -1,10 +1,16 @@
 package programming.set11.xyset;
 
+import java.util.LinkedList;
+
 /**
  * ExcellentXYSet
  */
 public class ExcellentXYSet<E> implements XYSet<E> {
+    private LinkedList<E> content;
 
+    public ExcellentXYSet() {
+        content = new LinkedList<E>();
+    }
 
     @Override
     public boolean add(E element) {
@@ -12,8 +18,39 @@ public class ExcellentXYSet<E> implements XYSet<E> {
             throw new IllegalArgumentException("The input was null.");
         }
 
-        if ()
+        if (this.contains(element)) {
+            return false;
+        }
 
+        // If the program got here, the element wasn't already in the set.
+        content.add(element);
+        return true;
+    }
+
+    @Override
+    public boolean remove(E element) {
+        if (element == null) {
+            throw new IllegalArgumentException("The input was null.");
+        }
+        return content.remove(element);
+    }
+
+    @Override
+    public boolean contains(E element) {
+        if (element == null) {
+            throw new IllegalArgumentException("The input was null.");
+        }
+        return content.contains(element);
+    }
+
+    @Override
+    public void clear() {
+        content.clear();
+    }
+
+    @Override
+    public int size() {
+        return content.size();
     }
 
 }
